7a84601c270f9f8ccb61e10b561d26de193cb845
Makokoo <35466175+Makokoo@users.noreply.github.com>
2018-05-18 14:26:52 +0200

    Foro v0.1

---
+++ forum.php
@@ -0,0 +1,175 @@
+<?php
+include_once('header.php');
+?>
+<hr>
+<div class="container">
+	<div class="row">
+  		<section class="panel panel-success">
+          <header class="panel-heading">
+            <h3>CATEGORÍA</h3>
+          </header>
+          
+          <section class="row panel-body">
+            <section class="col-md-6">
+               <h4> <a href="#" style="color:black"><i class="fa fa-commenting"> </i> NOMBRE HILO</a></h4>
+              
+            </section>
+            <section class="col-md-3">
+              <ul id="post-topic">
+                <li class="list-unstyled"> Topics:62 </li>
+                <li class="list-unstyled"> Posts:445 </li>
+              </ul>
+            </section>
+            
+           
+          </section>
+          
+          
+          
+      	</section>
+          
+  </div>
+
+
+  <div class="row">
+      <section class="panel panel-success">
+          <header class="panel-heading">
+            <h3>CATEGORÍA</h3>
+          </header>
+          
+          <section class="row panel-body">
+            <section class="col-md-6">
+               <h4> <a href="#" style="color:black"><i class="fa fa-commenting"> </i> NOMBRE HILO</a></h4>
+              
+            </section>
+            <section class="col-md-3">
+              <ul id="post-topic">
+                <li class="list-unstyled"> Topics:62 </li>
+                <li class="list-unstyled"> Posts:445 </li>
+              </ul>
+            </section>
+            
+           
+          </section>
+          
+          
+          
+        </section>
+          
+  </div>
+
+
+  <div class="row">
+      <section class="panel panel-success">
+          <header class="panel-heading">
+            <h3>CATEGORÍA</h3>
+          </header>
+          
+          <section class="row panel-body">
+            <section class="col-md-6">
+               <h4> <a href="#" style="color:black"><i class="fa fa-commenting"> </i> NOMBRE HILO</a></h4>
+              
+            </section>
+            <section class="col-md-3">
+              <ul id="post-topic">
+                <li class="list-unstyled"> Topics:62 </li>
+                <li class="list-unstyled"> Posts:445 </li>
+              </ul>
+            </section>
+            
+           
+          </section>
+          
+          
+          
+        </section>
+          
+  </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<section class="container">
+  <div class="row clearfix">
+    <div class="col-md-12 column">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          MENSAJE #1
+        </div>
+        <section class="row panel-body">         
+                  <section class="col-md-3">
+                      <section class="well">
+                            <h3 class='text-center'>NOMBRE</h3>
+                             
+                      </section>
+                  </section>
+                  <section class="col-md-9">
+                      <section class="well">
+                            <h3 class='text-left'>MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJEMENSAJE MENSAJE MENSAJEMENSAJE</h3>
+                             
+                      </section>
+                  </section>
+                  
+        </section>
+        
+      </div>
+    </div>
+  </div>
+          
+          
+          
+    </section>
+  </section>
+</section>
+
+<section class="container">
+  <div class="row clearfix">
+    <div class="col-md-12 column">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          MENSAJE #2
+        </div>
+        <section class="row panel-body">         
+                  <section class="col-md-3">
+                      <section class="well">
+                            <h3 class='text-center'>NOMBRE</h3>
+                             
+                      </section>
+                  </section>
+                  <section class="col-md-9">
+                      <section class="well">
+                            <h3 class='text-left'>MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJE MENSAJEMENSAJE MENSAJE MENSAJEMENSAJE</h3>
+                             
+                      </section>
+                  </section>
+                  
+        </section>
+        
+      </div>
+    </div>
+  </div>
+          
+          
+          
+    </section>
+  </section>
+</section>
+
+
+
+
+
+
+
+
+<?php
+include_once('footer.php');
