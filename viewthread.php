4bb32ecdcd53a0f883aebce091da1bb8d9634985
Makokoo <35466175+Makokoo@users.noreply.github.com>
2018-05-28 10:33:53 +0200

    foros con letra pequeñita

--- viewthread.php
+++ viewthread.php
@@ -2,7 +2,7 @@
 include_once ('funciones.php');
 include_once ('header.php');
 
-echo"<script type=\"text/javascript\">
+echo"<script type='text/javascript'>
 function ask(id){
  
     var r = confirm('¿Seguro que quieres borrar el mensaje?');
@@ -30,18 +30,18 @@ if(isset($_POST['message'])){
          $sql = "INSERT INTO mensajes (id_hilo,autor,mensaje) VALUES ($hilo,$autor,'$mensaje')";
         $conexion->query($sql);
         if ($conexion->affected_rows > 0) {
-            echo "<div class=\"container\" style='margin: 250px'>";
+            echo "<div class='container' style='margin: 250px'>";
             echo "<h2 class='text-center'>Mensaje enviado correctamente</h2>";
             echo "</div>";
             header("Refresh:3; url='viewthread.php?id=$hilo'");
         }else{
-            echo "<div class=\"container\" style='margin: 250px'>";
+            echo "<div class='container' style='margin: 250px'>";
             echo "<h2 class='text-center'>Mensaje no válido</h2>";
             echo "</div>";
             header("Refresh:3; url='viewthread.php?id=$hilo'");
         }
     }else{
-        echo "<div class=\"container\" style='margin: 250px'>";
+        echo "<div class='container' style='margin: 250px'>";
         echo "<h2 class='text-center' style='color:darkred'>El mensaje no puede estar vacio</h2>";
         echo "</div>";
         header("Refresh:3; url='viewthread.php?id=$hilo'");
@@ -51,7 +51,7 @@ if(isset($_POST['message'])){
 
 
     if (isset($_GET['id']) || isset($_POST['idhilo'])) {
-        echo "<div class=\"container\">";
+        echo "<div class='container'>";
         $mensajes = getMensajesHilo($_GET['id']);
         if (!isset($_POST['idhilo'])) {
             $idhilo = $_GET['id'];
@@ -67,10 +67,10 @@ if(isset($_POST['message'])){
         }
 
         echo "<h3 class='text-center' style='margin: 15px'> Estas viendo el hilo: <b>" . $primer['titulo'] . "</b></h3>";
-        echo "<div class=\"row clearfix\">
-    <div class=\"col-md-12 column\">
-      <div class=\"panel panel-default\">
-        <div class=\"panel-heading\">
+        echo "<div class='row clearfix'>
+    <div class='col-md-12 column'>
+      <div class='panel panel-default'>
+        <div class='panel-heading'>
           MENSAJE #1";
           if(isset($_SESSION['usuario'])){
             if($primer['autor'] == getid($_SESSION['usuario']) || getPermiso($_SESSION['usuario']) > 0){
@@ -80,17 +80,17 @@ if(isset($_POST['message'])){
             }
           }
         echo "</div>
-        <section class=\"row panel-body\">
-                  <section class=\"col-md-3\">
-                      <section class=\"well\">
+        <section class='row panel-body'>
+                  <section class='col-md-3'>
+                      <section class='well'>
                             <h3 class='text-center'><b>" . $nombre_usuario . "</b></h3>
-                            <img style='border-radius: 50%;' src=\"$avatar\">
+                            <img style='border-radius: 50%;' src='$avatar'>
 
                       </section>
                   </section>
-                  <section class=\"col-md-9\">
-                      <section class=\"well\">
-                            <h3 class='text-left'>" . $primer['mensaje'] . "</h3>
+                  <section class='col-md-9'>
+                      <section class='well'>
+                            <span class='text-left' style='color:black;'>" . $primer['mensaje'] . "</span>
 
                       </section>
                   </section>
@@ -114,10 +114,10 @@ if(isset($_POST['message'])){
             if (!$datos_usuario['avatar'] == null || !$datos_usuario['avatar'] == "") {
                 $avatar = "avatar/" . $datos_usuario['id_usuario'] . ".png";
             }
-            echo "<div class=\"row clearfix\">
-    <div class=\"col-md-12 column\">
-      <div class=\"panel panel-default\">
-        <div class=\"panel-heading\">
+            echo "<div class='row clearfix'>
+    <div class='col-md-12 column'>
+      <div class='panel panel-default'>
+        <div class='panel-heading'>
           MENSAJE #$contador";
           if(isset($_SESSION['usuario'])){
             if($mensajes[$i]['autor'] == getid($_SESSION['usuario']) || getPermiso($_SESSION['usuario']) > 0){
@@ -128,17 +128,17 @@ if(isset($_POST['message'])){
           }
 
         echo"</div>
-        <section class=\"row panel-body\">
-                  <section class=\"col-md-3\">
-                      <section class=\"well\">
+        <section class='row panel-body'>
+                  <section class='col-md-3'>
+                      <section class='well'>
                             <h3 class='text-center'><b>" . $nombre_usuario . "</b></h3>
-                            <img style='border-radius: 50%;' src=\"$avatar\">
+                            <img style='border-radius: 50%;' src='$avatar'>
 
                       </section>
                   </section>
-                  <section class=\"col-md-9\">
-                      <section class=\"well\">
-                            <h3 class='text-left'>" . $mensajes[$i]['mensaje'] . "</h3>
+                  <section class='col-md-9'>
+                      <section class='well'>
+                            <span class='text-left' style='color:black;'>" . $mensajes[$i]['mensaje'] . "</span>
 
                       </section>
                   </section>
@@ -152,21 +152,21 @@ if(isset($_POST['message'])){
         }
 
 if(isset($_SESSION['usuario'])) {
-    echo "<hr><div class=\"row clearfix\">
-    <div class=\"col-md-12 column\">
+    echo "<hr><div class='row clearfix'>
+    <div class='col-md-12 column'>
       
-        <div class=\"panel-heading\">
+        <div class='panel-heading'>
           NUEVA RESPUESTA
         </div>
         
                  
-<form action=\"viewthread.php\" method=\"post\" role=\"form\" class=\"contactForm\">
-							<div class=\"form-group\">
-								<textarea class=\"form-control input-text text-area\" name=\"message\" id=\"message\" ></textarea>
+<form action='viewthread.php' method='post' role='form' class='contactForm'>
+							<div class='form-group'>
+								<textarea class='form-control input-text text-area' name='message' id='message' ></textarea>
 							
 							</div>
                             <input type='hidden' name='idhilo' id='idhilo' value='$idhilo' >
-							<div class=\"text-center\"><button type=\"submit\" class=\"input-btn\">Enviar Respuesta</button></div>
+							<div class='text-center'><button type='submit' class='input-btn'>Enviar Respuesta</button></div>
 						</form>
 
         
@@ -179,10 +179,10 @@ if(isset($_SESSION['usuario'])) {
     echo "</div>";
 
 }else{
-    echo "<hr><div class=\"row clearfix\">
-    <div class=\"col-md-12 column\">
+    echo "<hr><div class='row clearfix'>
+    <div class='col-md-12 column'>
       
-        <div class=\"panel-heading\">
+        <div class='panel-heading'>
           DEBES INICIAR SESIÓN PARA PARTICIPAR EN LOS FOROS
         </div>
 
