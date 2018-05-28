36510415418170b17f291e6c7ebdd5461d801a67
Makokoo <35466175+Makokoo@users.noreply.github.com>
2018-05-28 13:44:32 +0200

    ahora muestra cuando un torneo está finalizado

--- admin/tournaments_admin.php
+++ admin/tournaments_admin.php
@@ -53,32 +53,40 @@ function askrondafinal(id){
         	/*
         		id_torneo,id_juego,estado,max_participantes,fec_inicio,fec_final,ganador,nombre_torneo,
         	*/
-        	if(count($datos_torneo) > 0){
-        		echo "<table class='table table-bordered text-center'>";
-        		echo "<th class='text-center'>ID</th><th class='text-center'>Juego</th><th class='text-center'>Nombre Torneo</th><th class='text-center'>Usuarios Inscritos</th><th class='text-center'>Estado</th><th class='text-center'>Acción</th>";
-        		for ($i=0; $i < count($datos_torneo); $i++) {
-        			$id = $datos_torneo[$i]['id_torneo'];
-        			$nombre = $datos_torneo[$i]['nombre_torneo'];
-        			$participantes = getparticipantes($id);
-        			$estado = $datos_torneo[$i]['estado'];
-        			$juego = $datos_torneo[$i]['id_juego'];
-        			$datos_juego = getDatosJuego($juego);
-        			$nombre_juego = $datos_juego['nombre'];
-        			echo "<tr><td>$id</td><td>$nombre_juego</td><td>$nombre</td><td>$participantes</td><td>$estado</td>";
-        			if($participantes == 8 && comprobarSiPartidasCreadas($id) == 0){
-        				echo "<td><button class='btn btn-success' onclick='ask($id)'>Iniciar Torneo</button></td>";
-        			}else if(comprobarPrimeraRondaJugada($id) == 4 && comprobarSiPartidasCreadas($id) < 5){
-                        echo "<td><button class='btn btn-warning' onclick='asksegundaronda($id)'>Iniciar Segunda Ronda</button></td>";
-                    }else if(comprobarSegundaRondaJugada($id) == 2 && comprobarSiPartidasCreadas($id) == 6){
-                        echo "<td><button class='btn btn-danger' onclick='askrondafinal($id)'>Iniciar Ronda Final</button></td>";
-                    }else{
-        				echo "<td>Jugando...</td>";
-        			}
+               if(count($datos_torneo) > 0){
+                  echo "<table class='table table-bordered text-center'>";
+                  echo "<th class='text-center'>ID</th><th class='text-center'>Juego</th><th class='text-center'>Nombre Torneo</th><th class='text-center'>Usuarios Inscritos</th><th class='text-center'>Estado</th><th class='text-center'>Acción</th>";
+                    for ($i=0; $i < count($datos_torneo); $i++) {
+                        $id = $datos_torneo[$i]['id_torneo'];
+                        $nombre = $datos_torneo[$i]['nombre_torneo'];
+                        $participantes = getparticipantes($id);
+                        $estado = $datos_torneo[$i]['estado'];
+                        $juego = $datos_torneo[$i]['id_juego'];
+                        $datos_juego = getDatosJuego($juego);
+                        $nombre_juego = $datos_juego['nombre'];
 
-        			echo "</tr>";
-        		}
-        		echo "</table>";
-        	}
+                        echo "<tr><td>$id</td><td>$nombre_juego</td><td>$nombre</td><td>$participantes</td><td>$estado</td>";
+                    
+                        if(hayganador($id) == false){
+                            if($participantes == 8 && comprobarSiPartidasCreadas($id) == 0){
+                                echo "<td><button class='btn btn-success' onclick='ask($id)'>Iniciar Torneo</button></td>";
+                            }else if(comprobarPrimeraRondaJugada($id) == 4 && comprobarSiPartidasCreadas($id) < 5){
+                                echo "<td><button class='btn btn-warning' onclick='asksegundaronda($id)'>Iniciar Segunda Ronda</button></td>";
+                            }else if(comprobarSegundaRondaJugada($id) == 2 && comprobarSiPartidasCreadas($id) == 6){
+                                echo "<td><button class='btn btn-danger' onclick='askrondafinal($id)'>Iniciar Ronda Final</button></td>";
+                            }else{
+                                echo "<td>Jugando...</td>";
+                            }
+                        }else{
+                            echo "<td style='color:darkred;'>Finalizado</td>";
+                        }
+
+                        echo "</tr>";
+                    }
+                
+                echo "</table>";
+                
+                }
 
         ?>
     </div>
